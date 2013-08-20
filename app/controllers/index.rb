3f019749d480bfe10651de1c82d172c10a407a90
Clay Whitley <clay.aaron.whitley@gmail.com>
2013-08-20 14:09:56 -0700

    finish mvp

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,24 @@
 get '/' do
   # Look in app/views/index.erb
+  redirect '/login' unless session[:user]
+  github = Github.new login: "#{session[:user][:username]}", password: "#{session[:user][:password]}"
+  @repos = github.repos.all per_page: 10
+  @gists = github.gists.all per_page: 10
+  activity = github.activity.events.user_performed user: "#{session[:user][:username]}"
+  @activity = activity.select { |action| action.type == "PushEvent"}
   erb :index
+end
+
+get '/login' do
+  erb :login
+end
+
+post '/login' do
+  session[:user] = {username: params[:username], password: params[:password]}
+  redirect '/'
+end
+
+get '/logout' do
+  session[:user] = nil
+  redirect '/'
 end
